c5f09359cfce0ee00e8a04ff1261ca78a172291e
abhishek <abhi3vish@gmail.com>
2024-12-15 22:27:57 +0530

    Resume Route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,9 +21,8 @@ function Navbar() {
     // Load the view count from localStorage when the component mounts
     const savedViews = parseInt(localStorage.getItem('educativeViews'), 10) || 0;
     setEducativeViews(savedViews);
-
-    
-    
+     
+     // Sirf Testing ke liye mushe allowed hai
     let uniqueID = localStorage.getItem('uniqueID');
     console.log(uniqueID);
     
@@ -68,7 +67,7 @@ function Navbar() {
             style={{ cursor: "pointer" }}
             onClick={() => {
               const uniqueID = localStorage.getItem("uniqueID");
-              if (uniqueID ==="ec584647-5f5b-453a-1138-9de4ca03ac33"|| uniqueID==="8d8217ea-332b-4cac-27f1-7b2459d4a6a5") {
+              if (uniqueID ==="ec584647-5f5b-453a-1138-9de4ca03ac33"|| uniqueID==="8d8217ea-332b-4cac-27f1-7b2459d4a6a5"||uniqueID==="bb8029b9-b265-4b40-318b-816f002153c4"||uniqueID==="447441dc-2355-4bec-13e9-71ad27f1e367") {
                 window.open(
                   "https://drive.google.com/file/d/1cA-MJMTx85oibfNxY2yx4LPDuGTSHoSY/view?usp=drive_link",
                   "_blank"

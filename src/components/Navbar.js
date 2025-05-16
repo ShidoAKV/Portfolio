50c560d1849625828e5c6538649b4f735500e315
abhishek <abhi3vish@gmail.com>
2025-05-17 00:18:35 +0530

    added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ function Navbar() {
   const handleEducativeClick = () => {
     if (educativeViews < 3) {
       window.open(
-        "https://drive.google.com/file/d/1Nvcrk8xjK7ANwXHoFYOZPTGle7ed7H9b/view?usp=drive_link",
+        "https://drive.google.com/file/d/1NfBVy4B0DpiZ_B0zIkovaQXfCts60zr_/view?usp=drive_link",
         '_blank'
       );
       const newViews = educativeViews + 1;
@@ -69,7 +69,7 @@ function Navbar() {
               const uniqueID = localStorage.getItem("uniqueID");
               if (uniqueID ==="ec584647-5f5b-453a-1138-9de4ca03ac33"|| uniqueID==="8d8217ea-332b-4cac-27f1-7b2459d4a6a5"||uniqueID==="bb8029b9-b265-4b40-318b-816f002153c4"||uniqueID==="447441dc-2355-4bec-13e9-71ad27f1e367") {
                 window.open(
-                  "https://drive.google.com/file/d/1Nvcrk8xjK7ANwXHoFYOZPTGle7ed7H9b/view?usp=drive_link",
+                  "https://drive.google.com/file/d/1NfBVy4B0DpiZ_B0zIkovaQXfCts60zr_/view?usp=drive_link",
                   "_blank"
                 );
               } else {
